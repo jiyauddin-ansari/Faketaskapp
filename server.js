07e6c9ca124792eec363cd7c110875e980d8df5a
jiyauddin-ansari <jiyauddindnb@gmail.com>
2025-08-04 01:53:32 +0530

    final changes

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+
+// const middlewares = jsonServer.defaults(); // Remove default static middleware that looks for /public
+
+const middlewares = jsonServer.defaults({
+  static: './nonexistent' // or false if using v0.17.0+
+});
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running → http://localhost:${PORT}`);
+});
